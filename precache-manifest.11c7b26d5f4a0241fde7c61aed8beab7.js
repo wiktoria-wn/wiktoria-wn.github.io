129eba87e09b97585c2624a9ed51c3eecc5ab6d1
wiktoria-wn <niezboralawiktoria@gmail.com>
2020-08-03 14:10:15 +0200

    Updates

---
+++ precache-manifest.11c7b26d5f4a0241fde7c61aed8beab7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c64b4a15dfacb13db48970bf53206de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08c5e18d4cd2f92a977f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "46560988d15f5d93c3fb",
+    "url": "/static/js/2.4ae35b59.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4ae35b59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08c5e18d4cd2f92a977f",
+    "url": "/static/js/main.f72e5ca6.chunk.js"
+  },
+  {
+    "revision": "8ff119494bfaf710e094",
+    "url": "/static/js/runtime-main.aa9fee99.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
